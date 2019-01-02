c54987900f1232a3a82553beec89e7e5f65745ec
Indira Williams <indirajamurath@gmail.com>
2019-01-02 17:35:13 -0500

    fix minor mistake in logic

--- array-merge-sort.js
+++ array-merge-sort.js
@@ -1,5 +1,5 @@
 function mergeSort(array) {
-  if(array.length === 0) return []
+  if(array.length < 2) return array
 	let sorted = []
   let max = Math.max(array[0], array[1])
   let min = Math.min(array[0], array[1])
@@ -39,5 +39,11 @@ return sorted
 }
 
 let arr = [8, 5, 2, 9, 5, 3, 6]
+let arr2 = [9, 3, 6, 7, 2, 1, 5, 3, 8]
 
-console.log(mergeSort([9, 3, 6, 7, 2, 1, 5, 3, 8]))
+console.log(mergeSort([]))
+console.log(mergeSort([5]))
+console.log(mergeSort([5, 1]))
+console.log(mergeSort([1, 11, 7]))
+console.log(mergeSort(arr))
+console.log(mergeSort(arr2))
